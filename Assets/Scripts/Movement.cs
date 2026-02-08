26bd09542ec3bd7d847720c89f10ed33ebad030a
Jman87 <juanjomartinezv2707@gmail.com>
2026-02-07 22:29:46 -0500

    Revert "Hour and 45 min mark of video"
    This reverts commit 298b6c292bfe9885cac2b9b2b8c6e602ce645f3b.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,63 +3,31 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {   
+    public Rigidbody2D Rigidbody { get; private set; }
     public float speed = 8.0f;
     public float speedMultiplier = 1.0f;
-    public Vector2 initialDirection;
-    public LayerMask WallsLayer; // for wall detection with raycast
-    public Rigidbody2D Rigidbody { get; private set; }
-    public Vector2 direction { get; private set; }
-    public Vector2 nextDirection { get; private set; }
-    public Vector3 startingPosition { get; private set; }
+    public LayerMask WallsLayer;// gonna use raycasts for checking walls
+
+    public Vector2 direction { get; private set;}
+
+    public Vector2 nextDirection { get; private set;}
     
-    private void Awake()
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake ()
     {
         this.Rigidbody = GetComponent<Rigidbody2D>();
-        this.startingPosition = this.transform.position;
-    }
-
-    private void Start()
-    {
-        ResetState();
-    }
-
-    private void Update()
-    {
-        if (this.nextDirection != Vector2.zero) {
-           SetDirection(this.nextDirection); 
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        Vector2 position = this.Rigidbody.position;
-        Vector2 translation = this.direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
-        this.Rigidbody.MovePosition(position + translation);
-    }
-
-    public void ResetState()
-    {
-        this.speedMultiplier = 1.0f;
-        this.direction = this.initialDirection;
-        this.nextDirection = Vector2.zero;
-        this.transform.position = this.startingPosition;
-        this.Rigidbody.bodyType = RigidbodyType2D.Dynamic;
-        this.enabled = true;
-    }
-
-    public void SetDirection(Vector2 direction, bool forced = false)
-    {
-        if (forced || !Occupied(direction)) {
-            this.direction = direction;
-            this.nextDirection = Vector2.zero;
-        } else {
-            this.nextDirection = direction;
-        }
-    }
-
-    public bool Occupied(Vector2 direction)
-    {
-        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.WallsLayer);
-        return hit.collider != null;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,35 +1,16 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Movement))]
 public class Player : MonoBehaviour
 {
-    public Movement movement { get; private set; }
-
-    private void Awake()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        this.movement = GetComponent<Movement>();
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        Vector2 direction = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            direction = Vector2.up;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-            direction = Vector2.down;
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            direction = Vector2.left;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            direction = Vector2.right;
-        }
-
-        if (direction != Vector2.zero)
-        {
-            this.movement.SetDirection(direction);
-        }
+        
     }
 }
